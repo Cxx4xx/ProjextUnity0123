cd5d82277728ee9b300eb2f37a4333f9df575beb
ly <619284064@qq.com>
2024-01-23 21:37:44 +0800

    test 1

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -7,12 +7,12 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("tes 1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
